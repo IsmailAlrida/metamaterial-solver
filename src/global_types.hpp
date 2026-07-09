5bdf8e1ef2878cb815d0a28b55bcc83e0b6cff1e
Alrida Ismail <alridaossama66@gmail.com>
2026-07-09 12:20:47 +0400

    [feat] added global type structs and enums

---
+++ src/global_types.hpp
@@ -0,0 +1,49 @@
+#pragma once
+#include <string>
+#include <vector>
+
+struct Result {
+    int code;
+    std::string msg;
+};
+
+enum solverDevices { 
+    serial,
+    parallel,
+    cuda
+};
+
+enum PhysicsProblems {
+
+    vibroacoustic,
+    electromagnetic
+}
+struct Solid {
+
+};
+
+struct Air {
+
+};
+
+struct SignalFFT {
+    int N;
+    std::vector<float> amplitude;
+    std::vector<float> phase;
+    std::vector<float> frequency;
+};
+
+struct SignalTD {
+    int size;
+    std::vector<float> amplitude;
+    std::vector<float> time;
+};
+
+struct SolverResult {
+    int success;
+    SignalFFT materialImpulseResponse;
+};
+
+struct SolverInput {
+    
+};
